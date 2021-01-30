78061757df64778bdef089e630fb28ddbc814d5d
andreybannikov <andrewnnov@yandex.ru>
2021-01-30 18:01:08 +0300

    use @DataProvider 201

--- src/main/java/Academy/HomePage.java
+++ src/main/java/Academy/HomePage.java
@@ -1,6 +1,7 @@
 package Academy;
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageobjects.LandingPage;
 import pageobjects.LoginPage;
@@ -10,8 +11,8 @@ import java.io.IOException;
 
 public class HomePage extends Base {
 
-    @Test
-    public void basePageNavigation() throws IOException {
+    @Test(dataProvider = "getData")
+    private void basePageNavigation(String userName, String password) throws IOException {
 
        driver = initializeDriver();
        driver.get("http://www.qaclickacademy.com");
@@ -20,8 +21,24 @@ public class HomePage extends Base {
         landingPage.getLogin().click();
 
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.getEmailAddress().sendKeys("aaaa@qa.com");
-        loginPage.getEmailAddress().sendKeys("bbb");
+        loginPage.getEmailAddress().sendKeys(userName);
+        loginPage.getUserPassword().sendKeys(password);
         loginPage.getLoginBtn().click();
+        //System.out.println(text);
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        Object[][] data = new Object[1][2];
+        data[0][0] = "nonrestricteduser@qa.com";
+        data[0][1] = "12345";
+        //data[0][2] = "Restricted user";
+
+        //data[1][0] = "restricted@qa.com";
+        //data[1][1] = "6789";
+        //data[1][2] = "Non restricted user";
+
+        return data;
+
     }
 }
